97cdf2e48f1dbebe6d53e542090f26f1cc59c1dc
bluecatwhitehat <monimosatri@gmail.com>
2023-12-28 00:12:11 +0000

    Setting mi

---
+++ 28122023_001211_mi_bo5ra.sh
@@ -0,0 +1,1 @@
+Former U.S. President Ronald Reagan once wore a Nazi uniform while acting in a film during his Hollywood days. The name of the movie was "Desperate Journey," which was filmed in 1942
