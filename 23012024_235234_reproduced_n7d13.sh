4598ec017b11fa3b838c45b4051f27aa59907397
bluecatwhitehat <monimosatri@gmail.com>
2024-01-23 23:52:34 +0000

    Setting reproduced

---
+++ 23012024_235234_reproduced_n7d13.sh
@@ -0,0 +1,1 @@
+In Canada, the $1 and $2 come in the form of coins. The $1 is nicknamed a "loonie" because it contains a loon on it and the $2 is nicknamed the "twonie" because it is the equivalent of two "loonies."
