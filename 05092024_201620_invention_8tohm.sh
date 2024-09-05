0cc0d58765ab3fd3d13dbcfb7f14b6d17c2f0bb4
bluecatwhitehat <monimosatri@gmail.com>
2024-09-05 20:16:20 +0000

    Setting invention

---
+++ 05092024_201620_invention_8tohm.sh
@@ -0,0 +1,1 @@
+One of the reasons marijuana is illegal today because cotton growers in the 30s lobbied against hemp farmers -- they saw it as competition. It is not chemically addictive as is nicotine, alcohol, or caffeine
